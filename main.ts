7cf10c1a950570242dd067411017e2eba9d8f340
shoushou <shoushou1106@icloud.com>
2025-04-01 13:22:26 -0700

    test: add log

--- main.ts
+++ main.ts
@@ -15,25 +15,46 @@ enum EnvVars {
   TOGETHER_API_KEY = "TOGETHER_API_KEY"
 }
 
+async function verifyDiscordRequest(req: Request, body: string) {
+  console.log("[Triggered] verifyDiscordRequest");
+
+  const signature = req.headers.get("X-Signature-Ed25519");
+  const timestamp = req.headers.get("X-Signature-Timestamp");
+  const discordPublicKey = Deno.env.get(EnvVars.DISCORD_PUBLIC_KEY);
+
+  if (typeof signature !== 'string' ||
+    typeof timestamp !== 'string' ||
+    typeof discordPublicKey !== 'string') {
+    return false;
+  }
+
+  return await verifyKey(body, signature, timestamp, discordPublicKey);
+}
+
 Deno.serve(
   { port: 8080 },
   async (req: Request) => {
+    console.log("[Triggered] Deno.serve");
     const body = await req.text();
-
+    
     if (!(await verifyDiscordRequest(req, body))) {
+      console.log("[Triggered] 401");
       return new Response("Invalid request signature", { status: 401 });
     }
 
     const message = JSON.parse(body);
 
     if (req.method === 'POST') {
+      console.log("[Triggered] POST request");
       switch (message.type) {
         case InteractionType.PING: {
+          console.log("[Triggered] PING");
           return new Response(JSON.stringify({
             type: InteractionResponseType.PONG
           }));
         }
         case InteractionType.APPLICATION_COMMAND: {
+          console.log("[Triggered] APPLICATION_COMMAND");
           switch (message.data.name.toLowerCase()) {
             case PING_COMMAND.name.toLowerCase(): {
               return new Response(JSON.stringify({
@@ -47,19 +68,6 @@ Deno.serve(
         }
       }
     }
+    console.log("[Triggered] 400");
     return new Response("Invalid request", { status: 400 });
 });
-
-async function verifyDiscordRequest(req: Request, body: string) {
-  const signature = req.headers.get("X-Signature-Ed25519");
-  const timestamp = req.headers.get("X-Signature-Timestamp");
-  const discordPublicKey = Deno.env.get(EnvVars.DISCORD_PUBLIC_KEY);
-
-  if (typeof signature !== 'string' ||
-    typeof timestamp !== 'string' ||
-    typeof discordPublicKey !== 'string') {
-    return false;
-  }
-
-  return await verifyKey(body, signature, timestamp, discordPublicKey);
-}
